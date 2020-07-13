c70852767fed2d87150d0f6c751859ebb67229a8
Michael Fellinger <michael.fellinger@iohk.io>
2020-07-13 03:48:55 +0200

    ensure was firing too soon

--- src/bitte/cli/rebuild.cr
+++ src/bitte/cli/rebuild.cr
@@ -39,7 +39,7 @@ module Bitte
             uid: instance.uid
           sleep(ch_count * 2) # this works around https://github.com/NixOS/nix/issues/3794
         end
-#
+        #
         cluster.asgs.each do |name, asg|
           asg.instances.each do |instance|
             next if skip?(name)
@@ -67,76 +67,35 @@ module Bitte
         end
       end
 
-      def substituters
-        [
-          "https://cache.nixos.org",
-          "https://hydra.iohk.io",
-          "https://manveru.cachix.org",
-        ]
-      end
-
-      def trusted_public_keys
-        [
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=",
-          "manveru.cachix.org-1:L5nJHSinfA2K5dDCG3KAEadwf/e3qqhuBr7yCwSksXo=",
-          "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=",
-        ]
-      end
-
       def parallel_copy(channel, name, ip, flake, flake_attr, uid, attempts = 10)
         logger = log.for(name)
 
         spawn do
           begin
-            logger.info { "Copying closure to #{name} (#{ip})" }
+            begin
+              logger.info { "Copying closure to #{name} (#{ip})" }
 
-            # sh! "nix", "copy",
-            #   "--substitute-on-destination",
-            #   "--to", "ssh://root@#{ip}",
-            #   cluster.nix
-            #
-            # sh! "rsync", args: [
-            #   "-e", (["ssh"] + SSH::COMMON_ARGS + ssh_key ).join(" "),
-            #   "-r", "#{flake}/", "root@#{ip}:source/",
-            # ]
-            #
-            # sh! "ssh", args: SSH::COMMON_ARGS + ssh_key + [
-            #   "root@#{ip}",
-            #   [
-            #     "#{cluster.nix}/bin/nix",
-            #     "--experimental-features",
-            #     "'flakes nix-command'",
-            #     "shell", "nixpkgs#git", "-c",
-            #     "#{cluster.nix}/bin/nix",
-            #     "--experimental-features",
-            #     "'flakes nix-command'",
-            #     "build",
-            #     "--substituters", "'#{ substituters.join(" ") }'",
-            #     "--trusted-public-keys", "'#{trusted_public_keys.join(" ")}'",
-            #     "'./source##{flake_attr}'"
-            #   ].join(" ")
-            # ]
+              sh! "nix", "copy",
+                "--substitute-on-destination",
+                "--to", "ssh://root@#{ip}",
+                "#{flake}##{flake_attr}",
+                logger: logger
 
-            sh! "nix", "copy",
-              "--substitute-on-destination",
-              "--to", "ssh://root@#{ip}",
-              "#{flake}##{flake_attr}",
-              logger: logger
+              logger.info { "Copied closure, starting nixos-rebuild ..." }
 
-            logger.info { "Copied closure, starting nixos-rebuild ..." }
+              sh! "nixos-rebuild", "switch",
+                "--target-host", "root@#{ip}",
+                "--flake", "#{flake}##{uid}",
+                logger: logger
 
-            sh! "nixos-rebuild", "switch",
-              "--target-host", "root@#{ip}",
-              "--flake", "#{flake}##{uid}",
-              logger: logger
-
-            logger.info { "finished." }
-          rescue ex
-            if attempts > 0
-              sleep [2,3,5,7,11].sample.seconds
-              parallel_copy(channel, name, ip, flake, flake_attr, uid, attempts - 1)
-            else
-              log.error(exception: ex) { "failed copying to #{name} (#{ip})" }
+              logger.info { "finished." }
+            rescue ex
+              if attempts > 0
+                sleep [2, 3, 5, 7, 11].sample.seconds
+                parallel_copy(channel, name, ip, flake, flake_attr, uid, attempts - 1)
+              else
+                log.error(exception: ex) { "failed copying to #{name} (#{ip})" }
+              end
             end
           ensure
             channel.send nil
