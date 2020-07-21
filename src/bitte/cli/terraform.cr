360c0a450b04cfc172d20647cd464cdaeb3f8e8f
Michael Fellinger <michael.fellinger@iohk.io>
2020-07-21 13:03:03 +0200

    enable bitte terraform to apply plans

--- src/bitte/cli/terraform.cr
+++ src/bitte/cli/terraform.cr
@@ -11,6 +11,19 @@ module Bitte
         sh! "nix", "build",
           "#{flake}#clusters.#{cluster}.terraform-output",
           "-o", "config.tf.json"
+
+        sh! "terraform", "plan", "-out", "#{cluster}.plan"
+
+        log.info { "applying the plan in 20 seconds!" }
+        log.info { "Press return to apply immediately, Ctrl-C to cancel" }
+
+        STDIN.read_timeout = 20
+        begin
+          STDIN.gets
+        rescue IO::TimeoutError
+        end
+
+        sh! "terraform", "apply", "#{cluster}.plan"
       end
 
       def flake
