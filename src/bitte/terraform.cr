876e267426789a1671c010c21df1e085fabce236
Michael Fellinger <michael.fellinger@iohk.io>
2020-08-14 09:04:37 +0200

    fix logging

--- src/bitte/terraform.cr
+++ src/bitte/terraform.cr
@@ -18,7 +18,9 @@ module Bitte
 
       creds["credentials"]["app.terraform.io"]["token"]
     rescue ex
-      log.error(exception: ex) { "Could not found Terraform credentials, make sure you ran `terraform login`" }
+      Log.for("terraform").error(exception: ex) {
+        "Could not found Terraform credentials, make sure you ran `terraform login`"
+      }
     end
 
     def self.headers
