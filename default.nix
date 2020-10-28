f2a7dd94c2adc4b55f5d0559d26a1868cf511731
Shay Bergmann <sbergmann@kreisys.co>
2020-10-28 12:37:25 -0400

    Refactor to use flake-utils simpleFlake

--- default.nix
+++ default.nix
@@ -1,55 +1,73 @@
-{ stdenv, makeWrapper, crystal, inclusive, nixFlakes, nixos-rebuild, openssh
-, awscli, gitMinimal, coreutils, gnugrep, terraform-with-plugins
-, consul, sops, libssh2, pkgconfig, cfssl, rsync, openssl, vault-bin }: {
-  bitte = let
-    inner = crystal.buildCrystalPackage {
-      pname = "bitte-cli";
-      version = "0.1.0";
-      format = "crystal";
+{ stdenv
+, makeWrapper
+, crystal
+, inclusive
+, nixFlakes
+, nixos-rebuild
+, openssh
+, awscli
+, gitMinimal
+, coreutils
+, gnugrep
+, terraform-with-plugins
+, consul
+, sops
+, libssh2
+, pkgconfig
+, cfssl
+, rsync
+, openssl
+, vault-bin
+}:
+let
+  inner = crystal.buildCrystalPackage {
+    pname = "bitte-cli";
+    version = "0.1.0";
+    format = "crystal";
 
-      # NOTE inclusive freezes under latest nix which is used by Hydra
-      #      uncomment this once it's solved...
-      # src = inclusive ./. [ ./shard.lock ./shard.yml ./src ];
-      src = ./.;
+    # NOTE inclusive freezes under latest nix which is used by Hydra
+    #      uncomment this once it's solved...
+    # src = inclusive ./. [ ./shard.lock ./shard.yml ./src ];
+    src = ./.;
 
-      buildInputs = [ libssh2 openssl ];
+    buildInputs = [ libssh2 openssl ];
 
-      shardsFile = ./shard.nix;
+    shardsFile = ./shard.nix;
 
-      crystalBinaries.bitte = {
-        src = "src/bitte.cr";
-        options = [ "--verbose" "--debug" ];
-      };
+    crystalBinaries.bitte = {
+      src = "src/bitte.cr";
+      options = [ "--verbose" "--debug" ];
     };
-
-    PATH = stdenv.lib.makeBinPath [
-      awscli
-      consul
-      coreutils
-      gitMinimal
-      gnugrep
-      nixFlakes
-      nixos-rebuild
-      openssh
-      sops
-      terraform-with-plugins
-      cfssl
-      rsync
-      vault-bin
-    ];
-
-  in stdenv.mkDerivation {
-    pname = inner.pname;
-    version = inner.version;
-
-    nativeBuildInputs = [ makeWrapper ];
-    src = inner;
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src/bin/bitte $out/bin/bitte
-      wrapProgram $out/bin/bitte \
-        --set PATH ${PATH}
-    '';
   };
+
+  PATH = stdenv.lib.makeBinPath [
+    awscli
+    consul
+    coreutils
+    gitMinimal
+    gnugrep
+    nixFlakes
+    nixos-rebuild
+    openssh
+    sops
+    terraform-with-plugins
+    cfssl
+    rsync
+    vault-bin
+  ];
+
+in
+stdenv.mkDerivation {
+  pname = inner.pname;
+  version = inner.version;
+
+  nativeBuildInputs = [ makeWrapper ];
+  src = inner;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src/bin/bitte $out/bin/bitte
+    wrapProgram $out/bin/bitte \
+      --set PATH ${PATH}
+  '';
 }
