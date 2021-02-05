2eceb09f7673393e192fc13424415b2f66fc4312
Shay Bergmann <shay.bergmann@iohk.io>
2021-02-04 19:54:26 -0500

    Add a rudimentary spec for reference

--- spec/bitte_spec.cr
+++ spec/bitte_spec.cr
@@ -1,25 +1,13 @@
 require "spec"
-require "../fixtures/*"
+require "../src/cli.cr"
 
 describe "arguments" do
-  context "when required" do
-    context "when given" do
-      it "should not raise an error" do
-        File.tempfile("test") do |io|
-          RequiredArgumentCommand.run(["abc"] of String, output: io)
-          io.rewind
-          io.gets_to_end.should eq "abc\n"
-        end
-      end
-    end
-
-    context "when not given" do
-      it "should raise an error" do
-        File.tempfile("test") do |io|
-          RequiredArgumentCommand.run([] of String, error: io)
-          io.rewind
-          io.gets_to_end.should eq "Argument required: aa".colorize(:red).to_s + "\n"
-        end
+  context "when not given" do
+    it "should express gratitude" do
+      File.tempfile("test") do |io|
+        Bitte::CLI.run([] of String, error: io)
+        io.rewind
+        io.gets_to_end.should eq "Danke\n"
       end
     end
   end
--- src/bitte.cr
+++ src/bitte.cr
@@ -1,37 +1,4 @@
-require "admiral"
-require "json"
-
-module Bitte
-  class TopologyNode
-    include JSON::Serializable
-
-    # property kms : String
-    property region : String
-    property name : String
-
-    @[JSON::Field(key: "privateIP")]
-    property private_ip : String
-  end
-end
-
-if ARGV == ["schön"]
-  puts "Danke sehr"
-  exit
-end
-
-if ARGV == ["sehr"]
-  puts "Danke schön"
-  exit
-end
-
-if ARGV.empty?
-  puts "Danke"
-  exit
-end
-
-require "./bitte/cli.cr"
-require "./bitte/cli/helper.cr"
-require "./bitte/**"
+require "./cli.cr"
 
 Log.setup_from_env(
   default_level: Log::Severity::Info,
