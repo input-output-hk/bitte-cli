015b985914bfa582ac04acc80e37ac399f842d4d
Shay Bergmann <shay.bergmann@iohk.io>
2021-03-02 10:47:11 -0500

    Some macro witchcraft

--- cli/tests/cli_test.rs
+++ cli/tests/cli_test.rs
@@ -2,128 +2,18 @@ mod common;
 
 use anyhow::Result;
 use assert_cmd::prelude::*;
-use predicates::prelude::*; // Used for writing assertions
+use common::*;
 use predicate::str::*;
+use predicates::prelude::*; // Used for writing assertions
 use std::process::Command;
 
-mod macros {
-    use paste::paste;
-
-    macro_rules! amacro {
-        ($($x:expr)*) => {
-            &[$(stringify!{$x}),*]
-        };
-    }
-    #[test]
-    fn test_macro() {
-        assert_eq!(amacro!(hello world), &["hello", "world"]);
-        assert_eq!(amacro!(hello "world"), &["hello", "\"world\""])
-    }
-
-    macro_rules! anothermacro {
-        ($($x:expr)*) => {
-            paste! {
-                stringify!{ [<$($x)_*>] }
-            }
-        };
-    }
-
-    #[test]
-    fn test_anothermacro() {
-        assert_eq!(anothermacro!(hello world), "hello_world");
-        assert_eq!(anothermacro!(hello "world"), "hello_world")
-    }
-}
-
-// macro_rules! bitte {
-//     () => (Command::cargo_bin("bitte")?);
-//     ($tf:expr) => { bitte!().arg($tf) };
-//     ($tf:expr, $network:expr) => (bitte!($tf).arg($network));
-//     ($tf:expr, $network:expr, $plan:expr) => (bitte!($tf).arg($network).arg($plan));
-// }
-
-macro_rules! bitte {
-    ($($args:expr),*) => {
-        Command::cargo_bin("bitte")?
-        $(
-            .arg($args)
-        )*
-    }
-}
+// I'm trying to abstract away as much boilerplate as possible.
+// For reference, a "normal" test looks like this:
 #[test]
-fn bitte_no_args_does_fail_stderr() -> Result<()> {
-    bitte!()
-        .assert()
-        .failure()
-        .stderr(contains("Unknown command"));
-
-    Ok(())
-}
-
-#[test]
-fn bitte_tf() -> Result<()> {
-    // cmd.env("BITTE_CLUSTER", "lies");
-    bitte!("tf")
-        .assert()
-        .failure()
-        .stderr(contains("bitte terraform <workspace>"));
-
-    Ok(())
-}
-
-#[test]
-fn bitte_tf_network() -> Result<()> {
-    // cmd.env("BITTE_CLUSTER", "lies");
-    // Command::cargo_bin("bitte")?
-    //     .args(&["tf", "network"])
-    bitte!("tf", "network")
-        .assert()
-        .failure()
-        .stderr(contains("Unknown command"));
-
-    Ok(())
-}
-
-#[test]
-fn bitte_tf_network_plan_given_noenv() -> Result<()> {
-    // cmd.env("BITTE_CLUSTER", "lies");
-    // Command::cargo_bin("bitte")?
-    //     .args(&["tf", "network", "plan"])
-    bitte!("tf", "network", "plan")
-        .assert()
-        .failure()
-        .stderr(contains("BITTE_CLUSTER"));
-
-    Ok(())
-}
-
-#[test]
-fn bitte_tf_network_plan_given_just_bitte_cluster() -> Result<()> {
-    // Command::cargo_bin("bitte")?
-    //     .args(&["tf", "network", "plan"])
-    bitte!("tf", "network", "plan")
-        .env("BITTE_CLUSTER", "lies")
-        .assert()
-        .failure()
-        .stderr(contains("TERRAFORM_ORGANIZATION"));
-
-    Ok(())
-}
-
-#[test]
-fn bitte_tf_network_plan_given_just_terraform_organization() -> Result<()> {
-    bitte!("tf", "network", "plan")
-        .env("TERRAFORM_ORGANIZATION", "lies")
-        .assert()
-        .failure()
-        .stderr(contains("BITTE_CLUSTER"));
-
-    Ok(())
-}
-
-#[test]
-fn bitte_tf_network_plan_given_both_bitte_cluster_and_terraform_organization() -> Result<()> {
-    bitte!("tf", "network", "plan")
+fn test_bitte_tf_network_plan_with_both_terraform_organization_and_bitte_cluster_set() -> Result<()> {
+    // ☝️ nobody wants to write this!
+    Command::cargo_bin("bitte")?
+        .args(&["tf", "network", "plan"])
         .env("TERRAFORM_ORGANIZATION", "lies")
         .env("BITTE_CLUSTER", "lies")
         .assert()
@@ -131,4 +21,65 @@ fn bitte_tf_network_plan_given_both_bitte_cluster_and_terraform_organization() -
         .stderr(contains("is not a flake"));
 
     Ok(())
+}
+
+// Using mod names for context saves some typing compared to fn name prefices.
+mod with_no_env {
+    use super::test;
+    // the test! macro is defined in common/mod.rs
+    // it currently supports only failures 🙃
+    test!(bitte; should fail with "Unknown command");
+    test!(bitte tf; should fail with "bitte terraform <workspace>");
+    test!(bitte tf network; should fail with "Unknown command"); // <- not really but it does.
+    test!(bitte tf network plan; should fail with "BITTE_CLUSTER");
+}
+
+mod with_bitte_cluster {
+    use super::test;
+    // We can give test! environment variables as well.
+    // I'd like to be able to scope that to the mod somehow.
+    test!(BITTE_CLUSTER="lies";
+          bitte;
+          should fail with "Unknown command");
+    test!(BITTE_CLUSTER="lies";
+          bitte tf;
+          should fail with "bitte terraform <workspace>");
+    test!(BITTE_CLUSTER="lies";
+          bitte tf network;
+          should fail with "Unknown command"); // <- not really but it does.
+    test!(BITTE_CLUSTER="lies";
+          bitte tf network plan;
+          should fail with "TERRAFORM_ORGANIZATION environment variable must be set");
+}
+
+mod with_terraform_organization {
+    use super::test;
+    test!(TERRAFORM_ORGANIZATION="moar_lies";
+          bitte;
+          should fail with "Unknown command");
+    test!(TERRAFORM_ORGANIZATION="moar_lies";
+          bitte tf;
+          should fail with "bitte terraform <workspace>");
+    test!(TERRAFORM_ORGANIZATION="moar_lies";
+          bitte tf network;
+          should fail with "Unknown command"); // <- not really but it does.
+    test!(TERRAFORM_ORGANIZATION="moar_lies";
+          bitte tf network plan;
+          should fail with "BITTE_CLUSTER environment variable must be set");
+}
+
+mod with_terraform_organization_and_bitte_cluster {
+    use super::test;
+    test!(BITTE_CLUSTER="lies" TERRAFORM_ORGANIZATION="moar_lies";
+          bitte;
+          should fail with "Unknown command");
+    test!(BITTE_CLUSTER="lies" TERRAFORM_ORGANIZATION="moar_lies";
+          bitte tf;
+          should fail with "bitte terraform <workspace>");
+    test!(BITTE_CLUSTER="lies" TERRAFORM_ORGANIZATION="moar_lies";
+          bitte tf network;
+          should fail with "Unknown command"); // <- not really but it does.
+    test!(BITTE_CLUSTER="lies" TERRAFORM_ORGANIZATION="moar_lies";
+          bitte tf network plan;
+          should fail with "is not a flake");
 }
