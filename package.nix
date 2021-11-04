803cae660839e457cabe80ac115355ffaa871e15
Timothy DeHerrera <tim.deherrera@iohk.io>
2021-11-04 09:12:16 -0600

    fix nix build

--- package.nix
+++ package.nix
@@ -5,7 +5,7 @@
 naersk.buildPackage {
   # Without this we end up with a drv called `rust-workspace-unknown`
   # which makes `nix run` try to execute a bin with that name.
-  inherit (with builtins; (fromTOML (readFile ./cli/Cargo.toml)).package)
+  inherit (with builtins; (fromTOML (readFile ./Cargo.toml)).package)
     name version;
   root = ./.;
   nativeBuildInputs = [ pkg-config ];
