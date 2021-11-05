c6f199911871885e69f15955c5b421f539d06ce9
David Arnold <david.arnold@iohk.io>
2021-11-05 02:58:17 -0500

    imp(build): #0 get rid of naersk
    - use fenix + `buildRustPackage`

--- package.nix
+++ package.nix
@@ -1,13 +1,22 @@
-{ stdenv, lib, pkg-config, openssl, zlib, rustPlatform, naersk
+{ stdenv, lib, pkg-config, openssl, zlib, makeRustPlatform, fenix
 # darwin dependencies
 , darwin }:
 
-naersk.buildPackage {
-  # Without this we end up with a drv called `rust-workspace-unknown`
-  # which makes `nix run` try to execute a bin with that name.
+(makeRustPlatform {
+  inherit (fenix.stable) cargo rustc;
+}).buildRustPackage {
+
   inherit (with builtins; (fromTOML (readFile ./Cargo.toml)).package)
-    name version;
-  root = ./.;
+    name
+    version
+  ;
+
+  src = ./.;
+  cargoLock.lockFile = ./Cargo.lock;
+  cargoLock.outputHashes = {
+    "deploy-rs-0.1.0" = "sha256-WFmUGwtV9mmQMAuenlRCGpJHJP2vP9MR/iTex+tGPtg=";
+  };
+
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl zlib ] ++ lib.optionals stdenv.isDarwin
     (with darwin.apple_sdk.frameworks; [
@@ -18,4 +27,4 @@ naersk.buildPackage {
       darwin.libresolv
       darwin.Libsystem
     ]);
-}
+} // { meta.description = "A swiss knife for the bitte cluster"; }
