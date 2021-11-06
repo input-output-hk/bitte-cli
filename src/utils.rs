7482fb062967bdd519d0eb2792bedf367a49d710
Timothy DeHerrera <tim.deherrera@iohk.io>
2021-11-06 10:32:46 -0600

    remove unused code and broken tests

--- src/utils.rs
+++ src/utils.rs
@@ -1,70 +1,9 @@
-pub mod certs;
 pub mod error;
 pub mod nomad;
-pub mod rebuild;
-pub mod ssh;
 pub mod terraform;
 pub mod types;
 
 use error::Error;
-
-use anyhow::{anyhow, Result};
-use execute::Execute;
-use log::debug;
-use std::process::Command;
-use std::process::Stdio;
-
-fn handle_command_error_common(
-    mut command: std::process::Command,
-    pipe_stdout: bool,
-) -> Result<String> {
-    debug!("run: {:?}", command);
-    if pipe_stdout {
-        command.stdout(Stdio::piped());
-    }
-    command.stderr(Stdio::piped());
-
-    match command.execute_output() {
-        Ok(output) => match output.status.code() {
-            Some(exit_code) => {
-                if exit_code == 0 {
-                    Ok(String::from_utf8_lossy(output.stdout.as_slice()).to_string())
-                } else {
-                    Err(Error::Exe {
-                        details: String::from_utf8_lossy(&output.stderr).to_string(),
-                    }
-                    .into())
-                }
-            }
-            None => Err(Error::Exe {
-                details: "interrupted".to_string(),
-            }
-            .into()),
-        },
-        Err(e) => Err(Error::Exe {
-            details: e.to_string(),
-        }
-        .into()),
-    }
-}
-
-fn handle_command_error(command: std::process::Command) -> Result<String> {
-    handle_command_error_common(command, false)
-}
-
-fn check_cmd(cmd: &mut Command) -> Result<()> {
-    println!("run: {:?}", cmd);
-    let status = cmd.status()?;
-    if status.success() {
-        Ok(())
-    } else {
-        Err(anyhow!(
-            "{:?} failed with non-zero exit code {:?}",
-            cmd,
-            status
-        ))
-    }
-}
 
 #[derive(Clone)]
 pub struct Instance {
