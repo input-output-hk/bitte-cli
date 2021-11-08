53d697b5f0c4a6cd328062229914c53d10095ff4
Timothy DeHerrera <tim.deherrera@iohk.io>
2021-11-07 19:12:26 -0700

    simplify main function
    We don't need to print error message to a buffer since clap has its own
    error handling code which runs automatically when an invalid subcommand
    is passed.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod cli;
-mod utils;
 mod types;
+mod utils;
 
-use anyhow::{bail, Result};
+use anyhow::Result;
 use clap::{App, IntoApp};
 use cli::opts::Bitte;
 use std::env;
@@ -13,15 +13,11 @@ use uuid::Uuid;
 async fn main() -> Result<()> {
     let _toml = include_str!("../Cargo.toml");
 
-    let mut app: App = <Bitte as IntoApp>::into_app()
+    let app: App = <Bitte as IntoApp>::into_app()
         .name(clap::crate_name!())
         .version(clap::crate_version!())
         .author(clap::crate_authors!("\n"))
         .about(clap::crate_description!());
-
-    let mut help_text = Vec::new();
-    app.write_help(&mut help_text)
-        .expect("Failed to write help text to buffer");
 
     let matches = app.get_matches();
 
@@ -35,16 +31,11 @@ async fn main() -> Result<()> {
     };
 
     match matches.subcommand() {
-        Some(("deploy", sub)) => cli::deploy(sub, run(false)).await,
-        Some(("info", sub)) => cli::info(sub, run(true)).await,
-        Some(("ssh", sub)) => cli::ssh(sub, run(true)).await,
-        Some(("terraform", sub)) => cli::terraform(sub, run(true)).await,
-        _ => {
-            bail!(format!(
-                "Invalid subcommand\n {}",
-                String::from_utf8(help_text).expect("help text contains invalid UTF8")
-            ))
-        }
-    }?;
+        Some(("deploy", sub)) => cli::deploy(sub, run(false)).await?,
+        Some(("info", sub)) => cli::info(sub, run(true)).await?,
+        Some(("ssh", sub)) => cli::ssh(sub, run(true)).await?,
+        Some(("terraform", sub)) => cli::terraform(sub, run(true)).await?,
+        _ => (),
+    }
     Ok(())
 }
