83fb848500d5fe26b2f790cd1e68abbabef94e73
Timothy DeHerrera <tim.deherrera@iohk.io>
2021-11-07 17:36:17 -0700

    enable IFD in bitteShell
    Needed for deploy-rs since is uses IFD to validate its JSON schema atm.

--- pkgs/bitte-shell.nix
+++ pkgs/bitte-shell.nix
@@ -18,6 +18,7 @@ in mkShell ({
 
   NIX_CONFIG = ''
     extra-experimental-features = nix-command flakes ca-references recursive-nix
+    allow-import-from-derivation = true
     extra-substituters = https://hydra.iohk.io
     extra-trusted-public-keys = hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=
     bash-prompt = \[\033[0;32m\][bitte]:\[\033[m\]\040
