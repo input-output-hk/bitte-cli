ea1811fbabdda9c40fb7d6bee198bcde2afa4618
David Arnold <david.arnold@iohk.io>
2021-11-11 01:38:33 -0500

    fixup devshell

--- devshellModule.nix
+++ devshellModule.nix
@@ -3,7 +3,13 @@ let
 
   mkStringOptionType = description: lib.mkOption {
     inherit description;
-    type = lib.types.string;
+    type = lib.types.str;
+  };
+
+  mkOptionalStringOptionType = description: lib.mkOption {
+    inherit description;
+    type = lib.types.nullOr lib.types.str;
+    default = null;
   };
 
   mkAttrsOptionType = description: lib.mkOption {
@@ -12,9 +18,8 @@ let
   };
 
   mkProviderOptionType = description: lib.mkOption {
-    k
-      inherit description;
-      type = lib.types.enum [ "AWS" ];
+    inherit description;
+    type = lib.types.enum [ "AWS" ];
   };
 
   cfg = config.bitte;
@@ -22,80 +27,80 @@ let
   asgRegionString = asg:
     let
       asgRegions = lib.attrValues
-        (lib.mapAttrs (_: v: v.region) asg
-          in
-        lib.strings.replaceStrings [ " " ] [ ":" ]
-        (toString asgRegions);
-
-      withCategory = category: attrset: attrset // { inherit category; };
-      infra = withCategory "infra";
-      app = withCategory "app";
-      utils = withCategory "utils";
-
+        (lib.mapAttrs (_: v: v.region) asg);
     in
-    {
+    lib.strings.replaceStrings [ " " ] [ ":" ]
+      (toString asgRegions);
 
-      options.bitte = {
-        cluster = mkStringOptionType "Name of the cluster";
-        domain = mkStringOptionType "Cluster root domain";
-        namespace = mkStringOptionType "Cluster main nomad namespace";
-        cert = mkStringOptionType "Certificate to authenticate with nomad, vault & consul";
-        provider = mkProviderOptionType "Infrastructure provider";
+  withCategory = category: attrset: attrset // { inherit category; };
+  infra = withCategory "infra";
+  app = withCategory "app";
+  utils = withCategory "utils";
 
-        aws_region = mkStringOptionType "AWS infrastructure region";
-        aws_profile = mkStringOptionType "AWS authentication profile";
-        aws_autoscaling_groups = mkAttrsOptionType "AWS auto scaling groups";
-      };
+in
+{
 
-      config = {
+  options.bitte = {
+    cluster = mkStringOptionType "Name of the cluster";
+    domain = mkStringOptionType "Cluster root domain";
+    namespace = mkOptionalStringOptionType "Cluster main nomad namespace";
+    cert = mkOptionalStringOptionType "Certificate to authenticate with nomad, vault & consul";
+    provider = mkProviderOptionType "Infrastructure provider";
 
-        name = cfg.cluster;
+    aws_region = mkStringOptionType "AWS infrastructure region";
+    aws_profile = mkStringOptionType "AWS authentication profile";
+    aws_autoscaling_groups = mkAttrsOptionType "AWS auto scaling groups";
+  };
 
-        commands = [
-          (infra { package = pkgs.bitte; })
-          (infra { package = pkgs.sops; })
-          (infra { package = pkgs.vault-bin; })
-          (infra { package = pkgs.consul; })
-          (infra { package = pkgs.awscli; })
-          (app { package = pkgs.iogo; })
-          (app { package = pkgs.nomad; })
-          (utils { package = pkgs.jq; })
-          (utils { package = pkgs.ijq; })
-          (utils { package = pkgs.fx; })
-          (utils { package = pkgs.curlie; })
-          (utils { package = pkgs.treefmt; })
-        ]
+  config = {
 
-          packages = with pkgs;
-        [
+    name = cfg.cluster;
+
+    commands = [
+      (infra { package = pkgs.bitte; })
+      (infra { package = pkgs.sops; })
+      (infra { package = pkgs.vault-bin; })
+      (infra { package = pkgs.consul; })
+      (infra { package = pkgs.awscli; })
+      (app { package = pkgs.iogo; })
+      (app { package = pkgs.nomad; })
+      (utils { package = pkgs.jq; })
+      (utils { package = pkgs.ijq; })
+      (utils { package = pkgs.fx; name = "fx"; })
+      (utils { package = pkgs.curlie; })
+      (utils { package = pkgs.treefmt; })
+    ];
+
+    packages = with pkgs;
+      [
         damon
-          scaler-guard
-          vault-bin
-          openssl
-          cfssl
-          nixfmt
-          consul-template
-          python38Packages.pyhcl
-          ];
+        scaler-guard
+        vault-bin
+        openssl
+        cfssl
+        nixfmt
+        consul-template
+        python38Packages.pyhcl
+      ];
 
-          env = [
-          { name = "BITTE_CLUSTER"; value = cfg.cluster; }
-          { name = "BITTE_DOMAIN"; value = cfg.domain; }
-          { name = "BITTE_PROVIDER"; value = cfg.provider; }
-          { name = "VAULT_ADDR"; value = "https://vault.${cfg.domain}"; }
-          { name = "NOMAD_ADDR"; value = "https://nomad.${cfg.domain}"; }
-          { name = "CONSUL_HTTP_ADDR"; value = "https://consul.${cfg.domain}"; }
-        ] ++ (lib.optionals (cfg.namespace != null) [
-          { name = "NOMAD_NAMESPACE"; value = cfg.namespace; }
-        ] ++ (lib.optionals (cfg.cert != null) [
-          { name = "CONSUL_CACERT"; value = cfg.cert; }
-          { name = "VAULT_CACERT"; value = cfg.cert; }
-          { name = "NOMAD_CACERT"; value = cfg.cert; }
-        ] ++ (lib.optionals (cfg.provider == "AWS") [
-          { name = "AWS_PROFILE"; value = cfg.aws_profile; }
-          { name = "AWS_DEFAULT_REGION"; value = cfg.aws_region; }
-          { name = "AWS_ASG_REGIONS"; value = asgRegionString cfg.aws_autoscaling_groups; }
-        ];
-          };
+    env = [
+      { name = "BITTE_CLUSTER"; value = cfg.cluster; }
+      { name = "BITTE_DOMAIN"; value = cfg.domain; }
+      { name = "BITTE_PROVIDER"; value = cfg.provider; }
+      { name = "VAULT_ADDR"; value = "https://vault.${cfg.domain}"; }
+      { name = "NOMAD_ADDR"; value = "https://nomad.${cfg.domain}"; }
+      { name = "CONSUL_HTTP_ADDR"; value = "https://consul.${cfg.domain}"; }
+    ] ++ (lib.optionals (cfg.namespace != null)) [
+      { name = "NOMAD_NAMESPACE"; value = cfg.namespace; }
+    ] ++ (lib.optionals (cfg.cert != null)) [
+      { name = "CONSUL_CACERT"; value = cfg.cert; }
+      { name = "VAULT_CACERT"; value = cfg.cert; }
+      { name = "NOMAD_CACERT"; value = cfg.cert; }
+    ] ++ (lib.optionals (cfg.provider == "AWS")) [
+      { name = "AWS_PROFILE"; value = cfg.aws_profile; }
+      { name = "AWS_DEFAULT_REGION"; value = cfg.aws_region; }
+      { name = "AWS_ASG_REGIONS"; value = asgRegionString cfg.aws_autoscaling_groups; }
+    ];
+  };
 
-          };
+}
