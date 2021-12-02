1d4d800033ee88b8f3204f7ff5ed6c7fe03c66b3
David Arnold <david.arnold@iohk.io>
2021-12-02 14:37:53 -0500

    rm: #0 remove superfluous nixConfig
    - use `nixConfig` flake.nix facilities, which since the last
      commit are guaranteed to be available within the nix shell

--- pkgs/bitte-shell.nix
+++ pkgs/bitte-shell.nix
@@ -9,7 +9,6 @@
 , profile ? ""
 , provider ? "AWS"
 , namespace ? cluster
-, nixConfig ? null
 , asg ? self.clusters.${cluster}.proto.config.cluster.autoscalingGroups
 }: devshell.mkShell {
   imports = [ bitteDevshellModule ];
@@ -19,13 +18,4 @@
   bitte.aws_region = region;
   bitte.aws_profile = profile;
   bitte.aws_autoscaling_groups = asg;
-  env = [{
-    name = "NIX_CONFIG";
-    value = ''
-      extra-experimental-features = nix-command flakes ca-references recursive-nix
-      allow-import-from-derivation = true
-      substituters = https://cache.nixos.org https://hydra.iohk.io
-      trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=
-    '' + (lib.optionalString (nixConfig != null) nixConfig);
-  }];
 }
